353d3c59187e42bcaf7686b054bf221d64563c43
Ketanag15 <Ketanag2k@gmail.com>
2025-03-07 00:28:02 +0530

    Updated classes

---
+++ src/main/java/com/shlokverse/implementation/LyricsServiceImpl.java
@@ -0,0 +1,45 @@
+package com.shlokverse.implementation;
+
+import com.shlokverse.model.Category;
+import com.shlokverse.model.Lyrics;
+import com.shlokverse.repository.LyricsRepository;
+import com.shlokverse.service.LyricsService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class LyricsServiceImpl implements LyricsService {
+
+    private final LyricsRepository lyricsRepository;
+
+    @Autowired
+    public LyricsServiceImpl(LyricsRepository lyricsRepository) {
+        this.lyricsRepository = lyricsRepository;
+    }
+
+    @Override
+    public List<Lyrics> getAllLyrics() {
+        return lyricsRepository.findAll();
+    }
+
+    @Override
+    public List<Lyrics> getLyricsByCategory(Category category) {
+        return lyricsRepository.findByCategory(category);
+    }
+
+    @Override
+    public Optional<Lyrics> getLyricsById(Long id) {
+        return lyricsRepository.findById(id);
+    }
+
+    @Override
+    public Lyrics saveLyrics(Lyrics lyrics) {
+        return lyricsRepository.save(lyrics);
+    }
+
+    @Override
+    public void deleteLyrics(Long id) {
+        lyricsRepository.deleteById(id);
+    }
+}
--- src/main/java/com/shlokverse/service/LyricsService.java
+++ src/main/java/com/shlokverse/service/LyricsService.java
@@ -1,4 +1,16 @@
 package com.shlokverse.service;
 
-public class LyricsService {
+import com.shlokverse.model.Category;
+import com.shlokverse.model.Lyrics;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LyricsService {
+    List<Lyrics> getAllLyrics();
+    List<Lyrics> getLyricsByCategory(Category category);
+    Optional<Lyrics> getLyricsById(Long id);
+    Lyrics saveLyrics(Lyrics lyrics);
+    void deleteLyrics(Long id);
+
 }
