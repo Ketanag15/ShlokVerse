94afd179cb5f237358d1580fa4b07dfd85571c20
Ketanag15 <Ketanag2k@gmail.com>
2025-03-07 00:22:01 +0530

    Updated classes

--- src/main/java/com/shlokverse/implementation/CategoryServiceImpl.java
+++ src/main/java/com/shlokverse/implementation/CategoryServiceImpl.java
@@ -1,5 +1,45 @@
 package com.shlokverse.implementation;
 
-public class CategoryServiceImpl {
+import com.shlokverse.model.Category;
+import com.shlokverse.model.God;
+import com.shlokverse.repository.CategoryRepository;
+import com.shlokverse.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+import java.util.Optional;
+
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public List<Category> getCategoriesByGod(God god) {
+        return categoryRepository.findByGod(god);
+    }
+
+    @Override
+    public Optional<Category> getCategoryById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    @Override
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
 }
