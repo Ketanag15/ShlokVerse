a2e9d5f66af41d4b91c39afbc2e38d9705c1e484
Ketanag15 <Ketanag2k@gmail.com>
2025-03-07 00:45:51 +0530

    implementation layer

--- src/main/java/com/shlokverse/controller/GodController.java
+++ src/main/java/com/shlokverse/controller/GodController.java
@@ -1,4 +1,29 @@
 package com.shlokverse.controller;
 
+import com.shlokverse.model.God;
+import com.shlokverse.service.GodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
+
 public class GodController {
+    @Autowired
+    private GodService godService;
+
+    @GetMapping
+    public ResponseEntity<List<God>> getAllGods(){
+        List<God> gods = godService.getAllGods();
+        return ResponseEntity.ok(gods);
+    }
+
+    @GetMapping
+    public ResponseEntity<God> getGodById(Long id){
+        Optional<God> god = godService.getGodById(id);
+        return god.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
 }
