a9b6c70ae10f142328807eb67b802c050d943452
Ketanag15 <Ketanag2k@gmail.com>
2025-03-12 00:35:05 +0530

    updated Lyrics Controller

--- src/main/java/com/shlokverse/controller/LyricsController.java
+++ src/main/java/com/shlokverse/controller/LyricsController.java
@@ -1,16 +1,51 @@
 package com.shlokverse.controller;
 
+import com.shlokverse.model.Category;
+import com.shlokverse.model.Lyrics;
+import com.shlokverse.service.CategoryService;
 import com.shlokverse.service.LyricsService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/lyrics")
 public class LyricsController {
     private final LyricsService lyricsService;
+    private final CategoryService categoryService;
 
-    public LyricsController(LyricsService lyricsService) {
+    @Autowired
+    public LyricsController(LyricsService lyricsService, CategoryService categoryService) {
         this.lyricsService = lyricsService;
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<Lyrics> getAllLyrics(){
+        return lyricsService.getAllLyrics();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Lyrics> getLyricsById(@PathVariable Long id){
+        return lyricsService.getLyricsById(id);
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public List<Lyrics> getLyricsByCategory(@PathVariable Long categoryId){
+        Optional<Category> category = categoryService.getCategoryById(categoryId);
+        return category.map(lyricsService::getLyricsByCategory).orElse(null);
+    }
+
+    @PostMapping
+    public Lyrics createLyrics(@RequestBody Lyrics lyrics){
+        return lyricsService.saveLyrics(lyrics);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLyrics(@PathVariable Long id){
+        lyricsService.deleteLyrics(id);
     }
 
 }
