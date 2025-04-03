274fda9a13b5cc01baac34710ed6713b074df649
Ketanag15 <Ketanag2k@gmail.com>
2025-04-03 23:54:46 +0530

    applying new approach

--- src/main/java/com/shlokverse/model/God.java
+++ src/main/java/com/shlokverse/model/God.java
@@ -1,48 +1,29 @@
 package com.shlokverse.model;
 
 import jakarta.persistence.*;
+
 import java.util.List;
 
-@Entity //this marks it as a table in the database.
-@Table(name = "gods") //specifies the table name as "gods".
-public class God {
-    @Id //Mark the ID as primary key of the gods table.
-    @GeneratedValue(strategy = GenerationType.IDENTITY)  //For Auto Incrementing the IDs.
-    private Long id;
+@Entity
+@Table(name = "God")
+public class God{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "god_id")
+    private Long godId;
 
-    @Column(nullable = false, unique = true) //Ensures no duplicate name of gods.
-    private String name;
+    @Column(name = "god_Name", nullable = false, unique = true)
+    private String godName;
 
-    @OneToMany(mappedBy = "god", cascade = CascadeType.ALL) //One god can have multiple categories like aarti, chalisa shlok.
+    // One-to-Many: One God can have many Lyrics entries
+    @OneToMany(mappedBy = "god", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Lyrics> lyrics;
 
-    public God() { }
+    public God(){
 
-    public God(String name){
-        this.name = name;
     }
 
-    public Long getId(){
-        return id;
-    }
-
-    public void setId(Long id){
-        this.id = id;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public List<Lyrics> getLyrics() {
-        return lyrics;
-    }
-
-    public void setLyrics(List<Lyrics> lyrics) {
-        this.lyrics = lyrics;
+    public God(String godName){
+        this.godName = godName;
     }
 }
